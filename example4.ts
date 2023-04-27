333b12527a766b1948e7fbf4e2799d094d1881a2
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-27 07:07:05 +0300

    Added Example4

---
+++ example4.ts
@@ -0,0 +1,13 @@
+let person = {
+  name: "Anıl",
+  age: 28,
+  hobbies: ["Sports", "Cooking"],
+  job: {
+    title: "Developer",
+    description: "TypeScript",
+  },
+}
+
+console.log(person.name)
+console.log(person.hobbies[0])
+console.log(person.job.title)
